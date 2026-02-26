2276c8252d64f03b8037b787bfe276241519ca1c
David Zhao <144359888+davidpyzhao@users.noreply.github.com>
2026-02-26 10:21:09 -0700

    java doc stuff

--- app/src/main/java/com/example/thevms/model/Event.java
+++ app/src/main/java/com/example/thevms/model/Event.java
@@ -25,6 +25,18 @@ public class Event {
 
     private HashMap<Entrant, Boolean> entrantList;
 
+    /**
+     * Constructor for the Event class.
+     * @param name The name of the event.
+     * @param description A description of the event.
+     * @param organizer The organizer of the event.
+     * @param location The location of the event.
+     * @param imageUrl The URL for the event's image.
+     * @param registrationStartTime The start time for registration.
+     * @param registrationEndTime The end time for registration.
+     * @param eventStartTime The start time of the event.
+     * @param eventEndTime The end time of the event.
+     */
     public Event(String name, String description, Organizer organizer, Location location, String imageUrl, Date registrationStartTime, Date registrationEndTime, Date eventStartTime, Date eventEndTime) {
         this.name = name;
         this.description = description;
@@ -40,90 +52,178 @@ public class Event {
         // Set the QR Code
     }
 
+    /**
+     * Gets the name of the event.
+     * @return The name of the event.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the event.
+     * @param name The new name of the event.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the description of the event.
+     * @return The description of the event.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the event.
+     * @param description The new description of the event.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets the organizer of the event.
+     * @return The organizer of the event.
+     */
     public Organizer getOrganizer() {
         return organizer;
     }
 
+    /**
+     * Sets the organizer of the event.
+     * @param organizer The new organizer of the event.
+     */
     public void setOrganizer(Organizer organizer) {
         this.organizer = organizer;
     }
 
+    /**
+     * Gets the location of the event.
+     * @return The location of the event.
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Sets the location of the event.
+     * @param location The new location of the event.
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Gets the image URL of the event.
+     * @return The image URL of the event.
+     */
     public String getImageUrl() {
         return imageUrl;
     }
 
+    /**
+     * Sets the image URL of the event.
+     * @param imageUrl The new image URL of the event.
+     */
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 
+    /**
+     * Removes the image URL of the event.
+     */
     public void removeImageUrl() {
         this.imageUrl = null;
     }
 
+    /**
+     * Gets the registration start time.
+     * @return The registration start time.
+     */
     public Date getRegistrationStartTime() {
         return registrationStartTime;
     }
 
+    /**
+     * Sets the registration start time.
+     * @param registrationStartTime The new registration start time.
+     */
     public void setRegistrationStartTime(Date registrationStartTime) {
         this.registrationStartTime = registrationStartTime;
     }
 
+    /**
+     * Gets the registration end time.
+     * @return The registration end time.
+     */
     public Date getRegistrationEndTime() {
         return registrationEndTime;
     }
 
+    /**
+     * Sets the registration end time.
+     * @param registrationEndTime The new registration end time.
+     */
     public void setRegistrationEndTime(Date registrationEndTime) {
         this.registrationEndTime = registrationEndTime;
     }
 
+    /**
+     * Gets the event start time.
+     * @return The event start time.
+     */
     public Date getEventStartTime() {
         return eventStartTime;
     }
 
+    /**
+     * Sets the event start time.
+     * @param eventStartTime The new event start time.
+     */
     public void setEventStartTime(Date eventStartTime) {
         this.eventStartTime = eventStartTime;
     }
 
+    /**
+     * Gets the event end time.
+     * @return The event end time.
+     */
     public Date getEventEndTime() {
         return eventEndTime;
     }
 
+    /**
+     * Sets the event end time.
+     * @param eventEndTime The new event end time.
+     */
     public void setEventEndTime(Date eventEndTime) {
         this.eventEndTime = eventEndTime;
     }
 
+    /**
+     * Gets the list of entrants for the event.
+     * @return A HashMap of entrants and their selection status.
+     */
     public HashMap<Entrant, Boolean> getEntrantList() {
         return entrantList;
     }
 
+    /**
+     * Adds an entrant to the event's entrant list.
+     * @param entrant The entrant to add.
+     * @param isSelected The selection status of the entrant.
+     */
     public void addEntrant(Entrant entrant, Boolean isSelected) {
         this.entrantList.put(entrant, isSelected);
     }
 
+    /**
+     * Removes an entrant from the event's entrant list.
+     * @param entrant The entrant to remove.
+     */
     public void removeEntrant(Entrant entrant) {
         this.entrantList.remove(entrant);
     }
--- app/src/main/java/com/example/thevms/model/Organizer.java
+++ app/src/main/java/com/example/thevms/model/Organizer.java
@@ -12,15 +12,38 @@ import java.util.List;
 public class Organizer extends Entrant {
     List<Event> currentEvents;
 
+    /**
+     * Constructor for the Organizer class.
+     * @param email The organizer's email.
+     * @param username The organizer's username.
+     * @param phoneNumber The organizer's phone number.
+     */
     public Organizer(String email, String username, String phoneNumber) {
         super(email, username, phoneNumber);
         this.currentEvents = new ArrayList<>();
     }
 
+    /**
+     * Creates a new event.
+     * @param startReg The start date for registration.
+     * @param endReg The end date for registration.
+     * @param startEvent The start date of the event.
+     * @param endEvent The end date of the event.
+     * @param name The name of the event.
+     * @param description A description of the event.
+     * @param location The location of the event.
+     * @param radius The radius of the event's location.
+     * @param maxEntrants The maximum number of entrants for the event.
+     */
     public void createEvent(Date startReg, Date endReg, Date startEvent, Date endEvent, String name, String description, Location location, Integer radius, Integer maxEntrants) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Sends a notification to a list of entrants for a specific event.
+     * @param entrants The list of entrants to notify.
+     * @param event The event for which to send a notification.
+     */
     public void sendNotification(List<Entrant> entrants, Event event) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
