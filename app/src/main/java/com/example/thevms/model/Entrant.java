b8d34c6f508c12153b08e8d837c60b3b87563183
David Zhao <144359888+davidpyzhao@users.noreply.github.com>
2026-02-26 09:39:37 -0700

    entrant class

--- app/src/main/java/com/example/thevms/model/Entrant.java
+++ app/src/main/java/com/example/thevms/model/Entrant.java
@@ -4,4 +4,39 @@ package com.example.thevms.model;
  * Represents an entrant in an event.
  */
 public class Entrant {
+    private String email;
+    private String username;
+    private String phoneNumber;
+    
+    public Entrant(String email, String username, String phoneNumber) {
+        this.email = email;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
--- app/src/main/java/com/example/thevms/model/User.java
+++ app/src/main/java/com/example/thevms/model/User.java
@@ -4,4 +4,5 @@ package com.example.thevms.model;
  * Represents a user of the application.
  */
 public interface User {
+    
 }
