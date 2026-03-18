156a439e4e63861e0459ca8af469892632dd97c6
Amar Kulovac <kulovac@ualberta.ca>
2026-03-18 15:14:43 -0600

    Created guidelines view

---
+++ app/src/main/java/com/example/thevms/ui/GuidelinesFragment.java
@@ -0,0 +1,44 @@
+package com.example.thevms.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.thevms.R;
+
+/**
+ * A fragment that displays the app guidelines.
+ */
+public class GuidelinesFragment extends Fragment {
+
+    public GuidelinesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guidelines, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button dismissButton = view.findViewById(R.id.btn_dismiss);
+        dismissButton.setOnClickListener(v -> {
+            if (getParentFragmentManager().getBackStackEntryCount() > 0) {
+                getParentFragmentManager().popBackStack();
+            } else {
+                requireActivity().onBackPressed();
+            }
+        });
+    }
+}
